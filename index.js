e753044b1420b080f466fd81ac5c6db6b4fa1b4e
Junran <jryang@terpmail.umd.edu>
2021-02-24 13:57:31 -0600

    changing transform name to dbtransform

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ export default function VegaTransformPostgres(params) {
   Transform.call(this, [], params);
 }
 
-VegaTransformPostgres.setHttpOptions = function(httpOptions) {
-  if(httpOptions) {
+VegaTransformPostgres.setHttpOptions = function (httpOptions) {
+  if (httpOptions) {
     this._httpOptions = httpOptions;
     return this;
   }
@@ -20,7 +20,7 @@ VegaTransformPostgres.setHttpOptions = function(httpOptions) {
 }
 
 VegaTransformPostgres.Definition = {
-  type: "postgres", // FixMe: make uppercase
+  type: "dbtransform", // FixMe: make uppercase
   metadata: { changes: true, source: true },
   params: [
     { name: "query", type: "string", required: true }
@@ -29,41 +29,48 @@ VegaTransformPostgres.Definition = {
 
 const prototype = inherits(VegaTransformPostgres, Transform);
 
-prototype.transform = async function(_, pulse) {
-  console.log(_)
-  if(!VegaTransformPostgres._httpOptions) {
+prototype.transform = async function (params, pulse) {
+  // console.log(params)
+  if (!VegaTransformPostgres._httpOptions) {
     throw Error("Vega Transform Postgres http options missing. Assign it with setHttpOptions.");
   }
-  if(!_.query) {
+  if (!params.query) {
     throw Error("Internal error: this._query should be defined");
   }
-  const result = await new Promise((resolve, reject) => {
-    const postData = querystring.stringify({
-      query: _.query
+
+  let result = [];
+  const postQuery = async function () {
+    const options = VegaTransformPostgres._httpOptions
+    const response = await fetch(options.url, {
+      method: options.method,
+      mode: options.mode,
+      headers: options.headers,
+      body: querystring.stringify({
+        query: params.query
+      })
     });
-    VegaTransformPostgres._httpOptions['Content-Length'] = Buffer.byteLength(postData);
-    const req = http.request(VegaTransformPostgres._httpOptions, res => {
-      let data = '';
-      res.on('data', (chunk) => {
-        data += chunk;
-      });
-      res.on('end', () => {
-        if(res.statusCode === 400) {
-          reject(`${res.statusMessage}: ${data}`);
-        } else {
-          resolve(JSON.parse(data));
-        }
-      });
-    });
-    req.on('error', err => {
-      reject(err);
-    });
-    req.write(postData);
-    req.end();
-  }).catch(err => {
-    console.error(err);
-    return [];
-  });
+
+    // fetch won’t reject on HTTP error status even if the response
+    // is an HTTP 404 or 500. Instead, it will resolve normally with
+    // ok status set to false
+    if (response.ok)
+      return await response.json();
+    else {
+      // capture the error message
+      const err = await response.json();
+      throw Error(
+        (err.error + ': ' + err.message).replace(/(\r\n|\n|\r)/gm, "")
+      );
+    }
+  };
+
+  try {
+    result = await postQuery();
+    // console.log(result)
+  } catch (error) {
+    console.log(error);
+  }
+
   result.forEach(ingest);
   const out = pulse.fork(pulse.NO_FIELDS & pulse.NO_SOURCE);
   this.value = out.add = out.source = out.rem = result;
